593acfffe97e1910977e2c1d66c6f3a32e611b37
Nikolay.Burnykov <nikolay.burnykov@orange.com>
2022-04-03 16:35:20 +0300

    commit

--- problem_01_matrix/problem_01_matrix.py
+++ problem_01_matrix/problem_01_matrix.py
@@ -2,41 +2,57 @@
 # leetcode problem https://leetcode.com/problems/01-matrix/
 ###########################################################################################
 
-from typing import List
+from typing import List, Optional
+from collections import deque
 
 
-def updateMatrix(mat: List[List[int]]) -> List[List[int]]:
+def updateMatrix2(mat: List[List[int]]) -> List[List[int]]:  # BFS based solution
+    if not mat:
+        return []
 
-    def get_min(mtx, i, j):
-        values = []
-        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-
-            if 0 <= x < len(mtx) and 0 <= y < len(mtx[0]):
-                values.append(mtx[x][y])
-
-        return min(values) + 1
-
-    if len(mat) == 0:
-        return mat
-
-    cells_to_reprocess = dict()
-    result = [[None] * len(mat[0]) for _ in mat]
+    visited_nodes = set()
+    q = deque()
 
     for i in range(len(mat)):
         for j in range(len(mat[0])):
-            result[i][j] = 0 if not mat[i][j] else get_min(mat, i, j)
-            if result[i][j] > 1:
-                cells_to_reprocess.setdefault(result[i][j], set()).add((i, j))
+            if mat[i][j] == 0:
+                visited_nodes.add((i, j))
+                q.append((i, j))
 
-    changed = True
-    while changed:
-        changed = False
-        ks = sorted(list(cells_to_reprocess.keys()))
-        for k in ks:
-            for i, j in cells_to_reprocess[k]:
-                candidate = get_min(result, i, j)
-                if candidate < result[i][j]:
+    while len(q) > 0:
+        i, j = q.popleft()
+        for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+            if 0 <= x < len(mat) and 0 <= y < len(mat[0]) and (x, y) not in visited_nodes:
+                mat[x][y] = mat[i][j] + 1
+                q.append((x, y))
+                visited_nodes.add((x, y))
+
+    return mat
+
+
+def updateMatrix(mat: List[List[int]]) -> List[List[int]]:
+    if not mat:
+        return []
+
+    result = [[float("inf")] * len(mat[0]) for _ in mat]
+
+    for i in range(0, len(mat)):
+        for j in range(0, len(mat[0])):
+            if mat[i][j] == 0:
+                result[i][j] = 0
+            else:
+                cand_a = result[i][j - 1] + 1 if j > 0 else float("inf")
+                cand_b = result[i - 1][j] + 1 if i > 0 else float("inf")
+                candidate = min(cand_a, cand_b)
+                if result[i][j] > candidate:
                     result[i][j] = candidate
-                    changed = True
+
+    for i in range(len(mat) - 1, -1, -1):
+        for j in range(len(mat[0]) - 1, -1, -1):
+            cand_a = result[i][j + 1] + 1 if j < len(mat[0]) - 1 else float("inf")
+            cand_b = result[i + 1][j] + 1 if i < len(mat) - 1 else float("inf")
+            candidate = min(cand_a, cand_b)
+            if result[i][j] > candidate:
+                result[i][j] = candidate
 
     return result
