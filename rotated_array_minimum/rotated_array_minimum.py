d650c7f8343c2ceeca839d17f0617a37f44aff46
Nikolay.Burnykov <nikolay.burnykov@orange.com>
2022-04-10 11:00:00 +0300

    commit

--- rotated_array_minimum/rotated_array_minimum.py
+++ rotated_array_minimum/rotated_array_minimum.py
@@ -7,16 +7,14 @@ def findMin(nums: List[int]) -> int:
         return 0
     if len(nums) == 1 or nums[0] < nums[-1]:
         return nums[0]
-    # if len(nums) == 2:
-    #     return min(nums[0], nums[1])
 
     l, r = 0, len(nums) - 1
     while l < r:
         mid = (l + r) // 2
 
-        if nums[mid] > nums[l] >= nums[0] :
+        if nums[mid] > nums[l]:
             l = mid
-        if nums[mid] < nums[r] <= nums[-1]:
+        if nums[mid] < nums[r]:
             r = mid
         if r - l == 1:
             return nums[r]
