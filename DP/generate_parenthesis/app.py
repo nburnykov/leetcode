05939fca4704be1067781e08c655a7441a5f56b4
Nikolay Burnykov <nikolay.burnykov@mysky.com>
2023-02-26 17:51:08 +0300

    update

--- DP/generate_parenthesis/app.py
+++ DP/generate_parenthesis/app.py
@@ -7,10 +7,10 @@ from typing import List
 
 def generateParenthesis(n: int) -> List[str]:
     result = [[""]]
-    for i in range(0, n):
+    for i in range(0, n):  # every next layer uses previous data i = last layer
         result.append([])
         for j in range(0, i + 1):
-            for r1 in result[j]:
-                for r2 in result[i - j]:
+            for r1 in result[j]:  # iterate through j layer
+                for r2 in result[i - j]: # addition
                     result[i + 1].append("(" + r1 + ")" + r2)
     return result[n]
--- LINKED_STRUCTURES/reverse_linked_list/app.py
+++ LINKED_STRUCTURES/reverse_linked_list/app.py
@@ -7,22 +7,19 @@ from typing import Optional
 
 from utils import ListNode
 
-
-def reverseList1(head: Optional[ListNode]) -> Optional[ListNode]:
-    if not head:
-        return
-
-    current = head
-    result = ListNode(current.val, None)
-    while current.next:
-        current = current.next
-        ln = ListNode(current.val, result)
-        result = ln
-    return result
-
-def reverseList(head: Optional[ListNode], prev=None) -> Optional[ListNode]:
+def reverseList1(head: Optional[ListNode], prev=None) -> Optional[ListNode]:
     if not head:
         return prev
     n = head.next
     head.next = prev
     return reverseList(n, head)
+
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    current_node, prev_node = head, None
+    while current_node:
+        next_node = current_node.next
+        current_node.next = prev_node
+        prev_node = current_node
+        current_node = next_node
+    return prev_node
